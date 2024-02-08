dfabb02c1b62430806d52333e4ad3c917bc8c11d
Asir Adnan <asiradnan23@gmail.com>
2024-02-08 22:41:57 +0600

    Email alert

--- asir.js
+++ asir.js
@@ -2,3 +2,6 @@ function navBarToggle(){
     document.getElementById("toplinks2").classList.toggle("toplinks2on");
     document.getElementById("toplinks2").classList.toggle("toplinks2off");
 }
+function email(){
+    alert("Email me at\nasiradnan23@gmail.com");
+}
