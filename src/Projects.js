305c1d4f6f3595c32a45b8e9cb400159704c3ae2
asiradnan <asiradnan23@gmail.com>
2024-07-30 16:28:17 +0600

    Updated with new links

--- src/Projects.js
+++ src/Projects.js
@@ -4,6 +4,8 @@ const tictactoeIcon = require("./pics/tictactoe.png")
 const calcIcon = require("./pics/calc.png")
 const weatherIcon = require("./pics/weather.jpg")
 const ytIcon = require("./pics/yt.jpeg")
+const urlIcon = require("./pics/url.png")
+const wishIcon = require("./pics/sendwish.png")
 function Projects(){
     return(
         <>
@@ -11,12 +13,34 @@ function Projects(){
         <div className="projectsMain">
             <div class="project">
                 <div class="pic">
+                    <img src={wishIcon}/>
+                </div>
+                <div class="about">
+                    <p id="p_head">Send Wishes</p>
+                    <p id="des">Send wishes to whoever you want without revealing your identity!</p>
+                    <a href="https://sendwish.asiradnan.com" target="_blank"><button>Send a Wish!</button></a>
+                    <a href="https://github.com/asiradnan/receiveemail" target="_blank"><button>Source Code</button></a>
+                </div>            
+            </div>   
+            <div class="project">
+                <div class="pic">
+                    <img src={urlIcon}/>
+                </div>
+                <div class="about">
+                    <p id="p_head">Chotto URL</p>
+                    <p id="des">URL Shortner to shorten to large urls into a fixed length. Effective to share large urls.</p>
+                    <a href="https://chottourl.asiradnan.com" target="_blank"><button>Use Now!</button></a>
+                    <a href="https://github.com/asiradnan/url-shortener" target="_blank"><button>Source Code</button></a>
+                </div>            
+            </div>   
+            <div class="project">
+                <div class="pic">
                     <img src={todoIcon}/>
                 </div>
                 <div class="about">
                     <p id="p_head">ToDo App</p>
                     <p id="des">Fully working minimal ToDo List webapp. Made with Django.</p>
-                    <a href="https://todobyasir.vercel.app/" target="_blank"><button>Use Now!</button></a>
+                    <a href="https://todo.asiradnan.com/" target="_blank"><button>Use Now!</button></a>
                     <a href="https://github.com/asiradnan/ToDo" target="_blank"><button>Source Code</button></a>
                 </div>            
             </div>   
@@ -27,7 +51,7 @@ function Projects(){
                 <div class="about">
                     <p id="p_head">Tic Tac Toe</p>
                     <p id="des">A game where you can play against your friend or AI!</p>
-                    <a href="https://asiradnan.github.io/Tic-Tac-Toe/" target="_blank"><button>Play Now!</button></a>
+                    <a href="https://tictactoe.asiradnan.com/" target="_blank"><button>Play Now!</button></a>
                     <a href="https://github.com/asiradnan/Tic-Tac-Toe" target="_blank"><button>Source Code</button></a>
                 </div>
             </div>
@@ -38,7 +62,7 @@ function Projects(){
                 <div class="about">
                     <p id="p_head">Calculator</p>
                     <p id="des">Basic calculator - result of vanilla javascript.</p>
-                    <a href="https://asiradnan.github.io/Calculator/" target="_blank"><button>Try Now!</button></a>
+                    <a href="https://calculator.asiradnan.com" target="_blank"><button>Try Now!</button></a>
                     <a href="https://github.com/asiradnan/Calculator" target="_blank"><button>Source Code</button></a>
                 </div>
             </div>
@@ -49,7 +73,7 @@ function Projects(){
                 <div class="about">
                     <p id="p_head">Weather</p>
                     <p id="des">A weather app with an integrated map.</p>
-                    <a href="https://asiradnan.github.io/Weather/" target="_blank"><button>Try Now!</button></a>
+                    <a href="https://weather.asiradnan.com" target="_blank"><button>Try Now!</button></a>
                     <a href="https://github.com/asiradnan/Weather" target="_blank"><button>Source Code</button></a>
                 </div>
             </div>
