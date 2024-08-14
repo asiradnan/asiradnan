32e0d76e7b16f706532ba93155857c4af1471769
asiradnan <asiradnan23@gmail.com>
2024-08-14 16:32:45 +0600

    Skill page updated

--- src/Projects.js
+++ src/Projects.js
@@ -6,11 +6,35 @@ const weatherIcon = require("./pics/weather.jpg")
 const ytIcon = require("./pics/yt.jpeg")
 const urlIcon = require("./pics/url.png")
 const wishIcon = require("./pics/sendwish.png")
+const qrIcon = require("./pics/qr.png")
+const copypasteIcon = require("./pics/copypaste.png")
 function Projects(){
     return(
         <>
         <h1>Web Projects</h1>
         <div className="projectsMain">
+            <div class="project">
+                <div class="pic">
+                    <img src={copypasteIcon}/>
+                </div>
+                <div class="about">
+                    <p id="p_head">Copy Paste</p>
+                    <p id="des">Easier way to send text or file from one device to another. </p>
+                    <a href="https://copypaste.asiradnan.com" target="_blank"><button>Try now!</button></a>
+                    <a href="https://github.com/asiradnan/copypaste" target="_blank"><button>Source Code</button></a>
+                </div>            
+            </div>   
+            <div class="project">
+                <div class="pic">
+                    <img src={qrIcon}/>
+                </div>
+                <div class="about">
+                    <p id="p_head">QR Code</p>
+                    <p id="des">Make customized qr code from any text or link!</p>
+                    <a href="https://qr.asiradnan.com" target="_blank"><button>Try QR!</button></a>
+                    <a href="https://github.com/asiradnan/qrcode" target="_blank"><button>Source Code</button></a>
+                </div>            
+            </div>   
             <div class="project">
                 <div class="pic">
                     <img src={wishIcon}/>
