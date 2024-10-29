950d09f133e24452db9a4ea837f8e6ed2b0aad6b
asiradnan <asiradnan23@gmail.com>
2024-10-29 12:54:31 +0600

    og meta tags

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,26 @@
 import {Inter} from "next/font/google";
 import "./globals.css";
 import Header from "@/app/components/Header";
+import logo_image from "@/public/pics/me_resized.png"
 
 const inter = Inter({ subsets: ['latin']  })
 
 export const metadata = {
   title: "Asir Adnan",
   description: "Landing page of Asir Adnan's portfolio",
+  openGraph: {
+    title: 'Asir Adnan\'s Portfolio',
+    description: 'Explore Asir Adnan\'s projects and professional experience.',
+    type: "website",
+    url: "https://asiradnan.com",
+    images: [
+      {
+        url: logo_image.src,
+        alt: "Asir Adnan's Profile Picture",
+      },
+    ],
+    site_name: "asiradnan.com"
+  },
 };
 
 export default function RootLayout({ children }) {
