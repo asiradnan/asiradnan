ef0cdf18eb43e23b5fc1cabcf8ba73d96fb68709
asiradnan <asiradnan23@gmail.com>
2024-11-20 20:23:18 +0600

    Skill Page and About font updated

--- app/about/page.js
+++ app/about/page.js
@@ -1,16 +1,24 @@
 import  "./About.css"
 import Link from "next/link";
+import { Caveat } from 'next/font/google';
 
 export const metadata = {
     title: "About - Asir Adnan",
   };
 
+  const caveat = Caveat({
+    subsets: ['latin'],
+    weight: ['400', '700'], // Adjust weights as needed
+});
 export default function About(){
     return (
-        <p id="about_paragraph">
-            Hey there! I&apos;m Asir Adnan, currently sailing through the exciting seas of computer science at BRAC University, where I&apos;m in my third year of pursuing a BS in Computer Science. <br/><br/>
-            I love <Link href="/cp">Competitive Programming</Link> which makes it easy for me to apply my &quot;liking to think&quot; attribute and my coding skills altogether. <br/><br/>
-            Also, I teach myself development tools to treat myself with delicious <Link href="/projects">Projects</Link>!
-        </p>
+        <div id="about_div" className={caveat.className}>
+            <h2>About Me</h2>
+            <p id="about_paragraph">
+                Hey there! I&apos;m Asir Adnan, currently sailing through the exciting seas of computer science at BRAC University, where I&apos;m in my third year of pursuing a BS in Computer Science. <br/><br/>
+                I love <Link href="/cp">Competitive Programming</Link> which makes it easy for me to apply my &quot;liking to think&quot; attribute and my coding skills altogether. <br/><br/>
+                Also, I teach myself development tools to treat myself with delicious <Link href="/projects">Projects</Link>!
+            </p>
+        </div>
     )
 }
--- app/skills/page.js
+++ app/skills/page.js
@@ -6,85 +6,88 @@ export const metadata = {
 
 export default function Skills(){
     return (
-        <div className="skillsMain">
-        <table>
-            <thead>
-                <tr>
-                    <th colSpan={2}>Programming</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Python</td><td>7/10</td>
-                </tr>
-                <tr>
-                    <td>C++</td><td>7/10</td>
-                </tr>
-                <tr>
-                    <td>Javascript</td><td>6/10</td>
-                </tr>
-                <tr>
-                    <td>Kotlin</td><td>3/10</td>
-                </tr>
-            </tbody>
-        </table>
-        <br></br>
-        <table>
-            <thead>
-                <tr>
-                    <th colSpan={2}>Web Technologies </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Django</td><td>7/10</td>
-                </tr>
-                <tr>
-                    <td>Django Rest Framework</td><td>5/10</td>
-                </tr>
-                <tr>
-                    <td>Database</td><td>7/10</td>
-                </tr>
-                <tr>
-                    <td>React JS</td><td>6/10</td>
-                </tr>
-                <tr>
-                    <td>Javascript</td><td>6/10</td>
-                </tr>
-                <tr>
-                    <td>HTML</td><td>8/10</td>
-                </tr>
-                <tr>
-                    <td>CSS</td><td>7/10</td>
-                </tr>
-                <tr>
-                    <td>Bootstrap</td><td>5/10</td>
-                </tr>
-                
-            </tbody>
-        </table>
-    {/* <div class='skill'>Programming 
-        <p> */}
-            {/* <span class="#best">Python</span>,
-            <span class="#best">C/C++</span>, 
-            <span class="#best">Javascript</span>, 
-            <span class="#better">Java</span>,
-            <span class="#good">Kotlin</span> */}
-            {/* Python, C/C++, Javascript, Java, Kotlin
-        </p>
-    </div>
-    <div class='skill'>Web Technologies 
-        <p> */}
-            {/* <span class="#best">HTML</span>,
-            <span class="#better">CSS</span>,
-            <span class="#better">Javascript</span>,
-            <span class="#best">Django</span>,
-            <span class="#better">MySQL</span>,
-            <span class="#good">Node JS</span>,
-            <span class="#good">JSON/API</span> */}
-            {/* HTML, CSS, Javascript, Django, MySQL, Node JS, JSON/API
-        </p>
-    </div> */}
-</div>
+        <>
+        
+        <header className="header">
+        <h1 className="title">My Skills</h1>
+    </header>
+
+    <main className="skills-container">
+        <section className="skill">
+            <h2>Frontend Development</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React.js</li>
+            </ul>
+        </section>
+
+        <section className="skill">
+            <h2>Backend Development</h2>
+            <ul>
+                <li>Django</li>
+                <li>Django Rest Framework</li>
+                <li>Node.js</li>
+                <li>NextJS</li>
+                <li>REST APIs</li>
+            </ul>
+        </section>
+
+        <section className="skill">
+            <h2>Programming Languages</h2>
+            <ul>
+                <li>Python</li>
+                <li>C++</li>
+                <li>C</li>
+                <li>Java</li>
+                <li>Kotlin</li>
+                <li>JavaScript</li>
+            </ul>
+        </section>
+
+        <section className="skill">
+            <h2>Programming Knowledge</h2>
+            <ul>
+                <li>Data Structures</li>
+                <li>Algorithm</li>
+                <li>OOP</li>
+                <li>Database Management (MySQL, MongoDB)</li>
+                <li>Software Design</li> 
+            </ul>
+        </section>
+
+        <section className="skill">
+            <h2>Soft Skills</h2>
+            <ul>
+                <li>Problem Solving</li>
+                <li>Critical Thinking</li>
+                <li>Time Management</li>
+                <li>Team Collaboration</li>
+                <li>Effective Communication</li>
+            </ul>
+        </section>
+
+        <section className="skill">
+            <h2>Tools & Technologies</h2>
+            <ul>
+                <li>Linux</li>
+                <li>Hosting</li>
+                <li>Git & GitHub</li>
+                <li>Docker</li>
+
+            </ul>
+        </section>
+
+        <section className="skill">
+            <h2>Other Skills</h2>
+            <ul>    
+                <li>UI/UX Basics</li>
+                <li>SEO Basics</li>
+            </ul>
+        </section>
+    </main>
+
+    </>
     )
 }
