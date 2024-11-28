5d25b5f25fa04ce8008f1b6f51bb852002a8d6b7
asiradnan <asiradnan23@gmail.com>
2024-11-28 19:12:55 +0600

    Updated styles for about, projects

--- app/about/page.js
+++ app/about/page.js
@@ -12,13 +12,19 @@ export const metadata = {
 });
 export default function About(){
     return (
-        <div id="about_div" className={caveat.className}>
-            <h2>About Me</h2>
+        <main className={caveat.className}>
+        <header className="header" >
+                <h1 className="title">About Me</h1>
+            </header>
+            <div id="about_div" >
+            
             <p id="about_paragraph">
                 Hey there! I&apos;m Asir Adnan, currently sailing through the exciting seas of computer science at BRAC University, where I&apos;m in my third year of pursuing a BS in Computer Science. <br/><br/>
                 I love <Link href="/cp">Competitive Programming</Link> which makes it easy for me to apply my &quot;liking to think&quot; attribute and my coding skills altogether. <br/><br/>
                 Also, I teach myself development tools to treat myself with delicious <Link href="/projects">Projects</Link>!
             </p>
         </div>
+        </main>
+        
     )
 }
