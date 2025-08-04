66138493600025d47d56deb6dbc5d3ffe882051c
asiradnan <asiradnan23@gmail.com>
2025-08-04 14:07:44 +0600

    optimized imports

--- app/page.js
+++ app/page.js
@@ -1,8 +1,5 @@
 import HeroSection from "../components/HeroSection";
 import AboutSection from "../components/AboutSection";
-import SkillsSection from "../components/SkillsSection";
-import ProjectsSection from "@/components/ProjectsSection";
-import AchievementsSection from "@/components/AchievementsSection";
 import ContactSection from "@/components/ContactSection";
 
 export const metadata = {
